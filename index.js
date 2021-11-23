59415adcfd0ab9108aab66668daf0b7f0015110b
Jeremy Bernabe <jeremy@interfused-inc.com>
2021-11-22 20:13:49 -0500

    grab detail

--- index.js
+++ index.js
@@ -93,8 +93,119 @@ async function getIfNextPageExists(page) {
 
 const grabFullDetail = async (url) => {
   console.log(`attempt grabFullDetail for: ${url}`);
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(url);
+  //wait for overview text
+  await page.waitForSelector("#pp_overview_text");
+
+  //click the toggle to expose full description
+  await page.click("a#read_more_toggle");
+
+  const companyDetails = await page.evaluate(() => {
+    let cleanAddressSegments = document.querySelector("p.pp_card_street");
+    let address1 = cleanAddressSegments
+      .querySelector("span.pp_card_street:nth-of-type(1)")
+      .innerText.trim();
+    let address2 = cleanAddressSegments
+      .querySelector("span.pp_card_street:nth-of-type(2)")
+      .innerText.trim();
+    let city = cleanAddressSegments
+      .querySelector("span:nth-of-type(3)")
+      .innerText.trim();
+    let state = cleanAddressSegments.querySelector(
+      "span:nth-of-type(4)"
+    ).innerText;
+    state = state.replace(",", "").trim();
+    let zip = cleanAddressSegments
+      .querySelector("span:nth-of-type(5)")
+      .innerText.trim();
+    let website = document
+      .querySelector("a.listing-desc-button")
+      .getAttribute("href");
+    if (website.includes("?")) {
+      website = website.split("?")[0];
+    }
+    let phone = document.querySelector(".svg-icon-phone + span").innerText;
+    let profilePhotoUrl = document
+      .querySelector("img#profile_photo_block")
+      .getAttribute("src");
+
+    let tmpEls = document.querySelectorAll(".card.more-info .block_content");
+    let moreBlocks = [];
+    console.log(`tmpEls len: ${tmpEls.length}`);
+
+    //convert html list to string list separated by a pipe (|)
+    const htmlListToStringList = (htmlStr) => {
+      let str = htmlStr.trim().replace(/>\s+</g, "><");
+
+      str = str.replaceAll("<ul><li>", "");
+      str = str.replaceAll("</li></ul>", "");
+      str = str.replaceAll("</li><li>", "|");
+      str = str.replaceAll(" |", "|");
+      str = str.replaceAll("| ", "|");
+      return str.trim();
+    };
+
+    //camelize string
+    function camelize(str) {
+      return str
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, "");
+    }
+
+    for (var el of tmpEls) {
+      let k = camelize(el.querySelector("h4").innerText);
+      let v = el.querySelector(".block_content_body").innerHTML;
+      //moreBlocks[k] = v;
+      let tmpObj = {};
+      //SKIP PHOTOS
+      if (!["photo", "address", "phone", "fax", "email"].includes(k)) {
+        //CLEAN LISTS TO STANDARD PIPE SEPARATED
+        if (
+          [
+            "practiceAreas",
+            "languages",
+            "classesAndSeminars",
+            "websites",
+          ].includes(k)
+        ) {
+          v = htmlListToStringList(v);
+        }
+
+        tmpObj[k] = v;
+        moreBlocks.push(tmpObj);
+      }
+    }
+
+    let fax = document.querySelector(".pplus_firm_fax").innerText;
+
+    return {
+      companyName: document.querySelector("h1.listing-details-header")
+        .innerText,
+      address1,
+      address2,
+      city,
+      state,
+      zip,
+      website,
+      phone,
+      fax,
+      profilePhotoUrl,
+      moreBlocks,
+      scrapeUrl: window.location.href,
+      description: document.querySelector("div#pp_overview_text").innerHTML,
+    };
+  });
+
+  console.log(`final details for the company were`);
+  console.log(companyDetails);
 };
 
+///BOTTOM IS WORKING BUT TEMPORARILY DISABLED
+/*
 scrape("ORLANDO", "FLORIDA").then((value) => {
   console.log(value);
   console.log("Collection length: " + value.length);
@@ -109,3 +220,8 @@ scrape("ORLANDO", "FLORIDA").then((value) => {
     grabFullDetail(unique[i]);
   }
 });
+*/
+
+grabFullDetail(
+  `https://lawyers.findlaw.com/profile/lawfirm/leighton-law-pa/fl/miami/NDgyNDE4MF8x/PP?refPageViewId=6b2560f8d45a3715`
+);

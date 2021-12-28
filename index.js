bbf6a70daa7e5d36cdb9fc88b7e4efaba14e16e5
Jeremy Bernabe <jeremy@interfused-inc.com>
2021-12-28 13:04:08 -0500

    complete states data

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ scrape("ventura County", "ALASKA").then((value) => {
 */
 
 let detailUrl =
-  "https://lawyers.findlaw.com/lawyer/firm/medical-malpractice/washington-county/rhode-island";
+  "https://lawyers.findlaw.com/lawyer/firm/medical-malpractice/weston-county/wyoming";
 
 scrapeDetaillUrl(detailUrl).then((value) => {
   console.log(value);
